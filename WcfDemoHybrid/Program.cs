5c0b1de6e7aa95c4d8d51604c5c3fcf843eb8569
snklim <sergey.klimenko@gmail.com>
2013-08-14 16:50:18 +0300

    Improves

--- WcfDemoHybrid/Program.cs
+++ WcfDemoHybrid/Program.cs
@@ -13,7 +13,7 @@ namespace WcfDemoHybrid
     {
         static void Main(string[] args)
         {
-            if(args.Length==0 || (args[0] != "-server" && args[0] != "-client"))
+            if (args.Length == 0 || (args[0] != "-server" && args[0] != "-client"))
             {
                 Console.WriteLine("Please use \"-server\" or \"-client\" as a parameter");
                 return;
@@ -31,14 +31,44 @@ namespace WcfDemoHybrid
 
         private static void RunServer()
         {
-            using (var serverHost = new ServiceHost(typeof(SimpleService), new Uri("http://localhost/simpleService")))
+            bool serverCreated = false;
+            string defUrl = Settings.DefaultServerEndpoint;
+
+            do
             {
-                serverHost.Open();
+                Console.Write("Enter server's endpoint (defailt is \"{0}\"):", defUrl);
+                string serverEndpoint = Settings.DefaultServerEndpoint;
 
-                Console.WriteLine("Server Started");
+                if (string.IsNullOrEmpty(serverEndpoint)) serverEndpoint = defUrl;
+                defUrl = serverEndpoint;
 
-                Console.ReadLine();
-            }
+                try
+                {
+                    using (var serverHost = new ServiceHost(typeof(SimpleService), new Uri(serverEndpoint)))
+                    {
+                        serverHost.Open();
+
+                        Console.WriteLine("Server started at " + serverEndpoint);
+
+                        serverCreated = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Exception currEx = ex;
+                    string msg = ex.Message;
+
+                    while (currEx.InnerException != null)
+                    {
+                        currEx = currEx.InnerException;
+                        msg += Environment.NewLine + currEx.Message;
+                    }
+
+                    Console.WriteLine("Error: \"{0}\". Please try again.", msg);
+                }
+            } while (!serverCreated);
+
+            Console.ReadLine();
         }
 
         private static void RunClient()
@@ -49,16 +79,22 @@ namespace WcfDemoHybrid
             {
                 Console.Write("Enter your name:");
                 clientName = Console.ReadLine();
+                if (string.IsNullOrEmpty(clientName)) Console.WriteLine("Empty name is not allowed. Please try again");
             } while (String.IsNullOrEmpty(clientName));
 
             var server = ChannelFactory<ISimpleService>.CreateChannel(
                             new BasicHttpBinding(),
-                            new EndpointAddress("http://localhost/simpleService"));
+                            new EndpointAddress(Settings.DefaultServerEndpoint));
 
             var clientService = new SimpleClient();
-            var url = "http://localhost/simpleClient" + "_" + clientService.ServiceUniqueName.ToString();
+            var url = Settings.DefaultClientEndpoint + "_" + clientService.ServiceUniqueName.ToString();
 
-            server.RegisterClient(new ClientDescription { ClientId = clientService.ServiceUniqueName, ServiceClietnCallbackUrl = url });
+            server.RegisterClient(new ClientDescription
+            {
+                ClientId = clientService.ServiceUniqueName,
+                ClientName = clientName,
+                ServiceClietnCallbackUrl = url
+            });
 
             using (var clientHost = new ServiceHost(clientService, new Uri(url)))
             {
@@ -73,11 +109,26 @@ namespace WcfDemoHybrid
         }
     }
 
+    public static class Settings
+    {
+        public static readonly string DefaultServerEndpoint;
+        public static readonly string DefaultClientEndpoint;
+
+        static Settings()
+        {
+            string hostName = "localhost:8081";
+            DefaultServerEndpoint = "http://" + hostName + "/simpleServer";
+            DefaultClientEndpoint = "http://" + hostName + "/simpleClient";
+        }
+    }
+
     [DataContract]
     public class ClientDescription
     {
         [DataMember]
         public string ClientId { get; set; }
+        [DataMember]
+        public string ClientName { get; set; }
         [DataMember]
         public string ServiceClietnCallbackUrl { get; set; }
     }
@@ -96,7 +147,9 @@ namespace WcfDemoHybrid
     public class SimpleService : ISimpleService
     {
         string _serviceName;
+        
         Dictionary<string, ISimpleClient> _clients;
+        Dictionary<string, ClientDescription> _clientsDescr;
 
         public string ServiceUniqueName { get { return _serviceName; } }
 
@@ -104,20 +157,23 @@ namespace WcfDemoHybrid
         {
             _serviceName = Guid.NewGuid().ToString();
             _clients = new Dictionary<string, ISimpleClient>();
+            _clientsDescr = new Dictionary<string, ClientDescription>();
         }
 
         public void SendMessage(string clientId, string message)
         {
-            Console.WriteLine(clientId + ": " + message);
+            Console.WriteLine(_clientsDescr[clientId].ClientName + ": " + message);
             foreach (var id in _clients.Keys)
             {
                 if (id == clientId) continue;
-                _clients[id].SendMessage(id, message);
+                _clients[id].SendMessage(_clientsDescr[id].ClientName, message);
             }
         }
 
         public void RegisterClient(ClientDescription clientDescr)
         {
+            Console.WriteLine("User \"{0}\" joined", clientDescr.ClientName);
+            _clientsDescr[clientDescr.ClientId] = clientDescr;
             _clients[clientDescr.ClientId] = ChannelFactory<ISimpleClient>.CreateChannel(
                                 new BasicHttpBinding(),
                                 new EndpointAddress(clientDescr.ServiceClietnCallbackUrl));
